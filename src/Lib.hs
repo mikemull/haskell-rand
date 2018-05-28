71b3ae656d925136766bd358a602d10ba71a5203
Mike Mull <mike.mull@gmail.com>
2018-05-28 16:43:57 -0700

    Add Point type

--- src/Lib.hs
+++ src/Lib.hs
@@ -6,6 +6,7 @@ module Lib
 import System.Random
 import Text.Printf
 
+data Point = Pt {px, py :: Float}
 
 someFunc :: IO ()
 someFunc = printf "%.4f\n" $ avgDist 1 (3/5)
@@ -24,13 +25,13 @@ avgDist a b = 1/15 * ((a^3/b^2) + (b^3/a^2) + ac * (3-a^2/b^2 - b^2/a^2))
 archosh :: Float -> Float
 archosh t = log (t + sqrt (t^2 - 1.0))
 
-dist (x1, x2) = sqrt ((fst x1 - fst x2)^2 + (snd x1 - snd x2)^2)
+dist (x1, x2) = sqrt ((px x1 - px x2)^2 + (py x1 - py x2)^2)
 
 avgDistMC :: StdGen -> Float -> Float -> Float
 avgDistMC sg a b = sum (map dist (take 1000 (recPoints (rndFromRec sg a b)))) / 1000
 
-recPoints :: [(Float, Float)] -> [((Float, Float), (Float, Float))]
-recPoints (q:r:xs) = (q,r): recPoints xs
+recPoints :: [(Float, Float)] -> [(Point, Point)]
+recPoints (q:r:xs) = (Pt (fst q) (snd q), Pt (fst r) (snd r)): recPoints xs
 
 rndFromRec :: StdGen -> Float -> Float -> [(Float, Float)]
 rndFromRec sg a b = zip x y
